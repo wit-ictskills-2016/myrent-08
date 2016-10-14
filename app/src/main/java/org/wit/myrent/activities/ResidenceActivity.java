965838881ad7bb87c0949a76b0c897f970b46431
John J. Fitzgerald <usplitu@gmail.com>
2016-10-14 11:34:39 +0100

    removed support action bar material

--- app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
@@ -6,7 +6,6 @@ import org.wit.myrent.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 public class ResidenceActivity extends AppCompatActivity
